de7125ea358e200e1cec63bde6352ff8d5be91a0
Sara <sgee03@gmail.com>
2016-04-08 11:45:05 -0700

    Require crypto. Create presave function

--- server/journey/journeyModel.js
+++ server/journey/journeyModel.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
+var Q = require('q');
 
 var JourneySchema = new mongoose.Schema({
   startPoint: {
@@ -12,6 +14,19 @@ var JourneySchema = new mongoose.Schema({
   hash: {
     type: String
   }
+});
+
+var createSha = function (points) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(points);
+  return shasum.digest('hex').slice(0, 5);
+};
+
+JourneySchema.pre('save', function(next) {
+  var journey = this;
+  var hash = createSha(journey.startPoint + journey.endPoint);
+  journey.hash = hash;
+  next();
 });
 
 module.exports = mongoose.model('Journey', JourneySchema);

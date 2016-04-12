60f87386dd62ede4db9224ec9f5846a6d3bbdaa5
Amanda Pattridge <alpattridge@gmail.com>
2016-04-12 13:55:37 -0700

    add route retreiveal in matCtrl and mapFactory

--- client/app/mapCtrl.js
+++ client/app/mapCtrl.js
@@ -3,6 +3,7 @@ angular.module('roadtrippin.maps', ['gservice'])
     $scope.route = {};
     $scope.route.stopOptions = [1, 2, 3, 4, 5];
     $scope.places = [];
+    $scope.savedRoutes = [];
 
     //this is a call to our Google maps API factory for directions
     $scope.getRoute = function() {
@@ -10,14 +11,30 @@ angular.module('roadtrippin.maps', ['gservice'])
         .then(function(places) {
           $scope.places = [];
           places.forEach(function(place) {
-            console.log(place);
+            place.address = place.address.split(', ')
+            console.log(place.name);
             $scope.places.push(place);
           });
         });
     };
 
     $scope.saveRoute = function () {
-      mapFactory.saveJourneyWithWaypoints(gservice.thisTrip);
+      mapFactory.saveJourneyWithWaypoints(gservice.thisTrip).then($scope.getAll);
     };
 
+    $scope.getAll = function () {
+      mapFactory.getAllRoutes().then(function(results){
+        $scope.savedRoutes = results;
+      })
+    }
+
+    $scope.viewSavedRoute = function (hash) {
+      for(var i = 0; i < $scope.savedRoutes.length; i++){
+        if($scope.savedRoutes[i].hash === hash){
+          gservice.render($scope.saveRoutes[i].startPoint, $scope.saveRoutes[i].endPoints, $scope.saveRoutes[i].wayPoints)
+        }
+      }
+    }
+
+    $scope.getAll();
   });

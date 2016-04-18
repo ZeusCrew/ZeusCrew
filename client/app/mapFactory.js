6cfc92878dea2ac741755c5239a4e3cc28d50271
natoen <hgmatsuda@gmail.com>
2016-04-18 10:26:34 +0900

    (feat) Add signout function

--- client/app/mapFactory.js
+++ client/app/mapFactory.js
@@ -17,12 +17,12 @@ angular.module('roadtrippin.mapsFactory', [])
       return deferred.promise;
     };
 
-    var getAllRoutes = function(){
+    var getAllRoutes = function() {
       var deferred = $q.defer();
       $http({
         method: 'GET',
         url: '/saveJourney'
-      }).then(function (res){
+      }).then(function (res) {
         deferred.resolve (res.data);
       }).catch(function (err) {
         deferred.reject (err);
@@ -30,8 +30,14 @@ angular.module('roadtrippin.mapsFactory', [])
       return deferred.promise;
     };
 
+    var signout = function() {
+      $window.localStorage.removeItem('com.roadtrippin');
+      $location.path('/signin');
+    };
+
     return {
       saveJourneyWithWaypoints: saveJourneyWithWaypoints,
-      getAllRoutes: getAllRoutes
+      getAllRoutes: getAllRoutes,
+      signout: signout
     };
   });
